b080a5c1c2b9cc176bc48b3e5d8dae49de6b3fd8
Nasir Hussain <72304678+nasir321@users.noreply.github.com>
2021-02-26 12:08:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#pip install opencv_python
+#must install openCV library before run.
 import cv2
 
-img = cv2.imread('opencv.png',)
-img = cv2.resize(img, (0, 0), fx= 1, fy=1)
-#img = cv2.rotate(img, cv2.cv2.ROTATE_180)
-cv2.imshow('Image', img)
+img = cv2.imread('opencv.png',) #load or read image
+img = cv2.resize(img, (0, 0), fx= 1, fy=1) #resize image 
+img = cv2.rotate(img, cv2.cv2.ROTATE_180) #Rotate image as per need
+cv2.imshow('Image', img) #Show image on screen
 cv2.waitKey(0)
 cv2.destroyWindow()
